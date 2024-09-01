f562387cf4e2fb662accda21f9820cf7ac3a8ce8
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2024-09-01 11:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Python file (app.py)
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,12 +5,33 @@ app = Flask(__name__)
 def hello_world():
     return '''
 <html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: #333;
+            font-size: 36px;
+        }
+        h2 {
+            color: #555;
+            font-size: 28px;
+        }
+        center {
+            margin-top: 50px;
+        }
+    </style>
+</head>
 <body>
 <center>
 <h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
 <br>
-<h2> deploying.. </h2>
+<h2>deploying..</h2>
 </center>
 </body>
 </html>
 '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
